7495dff47be9f0e9218c0d820f26eeb8106eae40
B Lub <brandonlublin@D-10-19-52-254.dhcp4.washington.edu>
2019-05-21 20:01:24 -0700

    all javascript is working, but my routes aren't working properly

---
+++ app/routing/apiRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+// Sets up the Express app to handle data parsing
+router.get('/friends', function(req,res) {
+
+})
+
+router.post('/friends', function(req,res) {
+    
+});
+
+module.exports = router; 
---
+++ app/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const apiRoutes = require('./apiRoutes');
+
+const app = express();
+
+app.use('/api', apiRoutes);
+app.use(express.static('resources'));
