1bd060d2923ec14d9a227ff17c49f2a2cf7fccb7
B Lub <brandonlublin@brandons-MacBook-Pro.local>
2019-05-28 15:12:56 -0700

    fixed routing, app should work on heroku now. server.js wasn't in root folder

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const htmlRoutes = require('./routing/htmlRoutes');
-const apiRoute = require('./routing/apiRoute');
+const htmlRoutes = require('./app/routing/htmlRoutes');
+const apiRoute = require('./app/routing/apiRoute');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('./app/public'))
+app.use(express.static('../app/public'))
 
 app.use('/', htmlRoutes);
 app.use('/api', apiRoute);
